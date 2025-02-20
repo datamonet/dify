8cf8124eb56efbc453e8e46c9e1b0014a20609a1
faye <faye_1225@163.com>
2025-02-20 21:57:08 +0800

    fix:dify web use hook error

--- web/app/(commonLayout)/app/(appDetailLayout)/[appId]/overview/page.tsx
+++ web/app/(commonLayout)/app/(appDetailLayout)/[appId]/overview/page.tsx
@@ -3,23 +3,23 @@ import ChartView from './chartView'
 import CardView from './cardView'
 import TracingPanel from './tracing/panel'
 import ApikeyInfoPanel from '@/app/components/app/overview/apikey-info-panel'
-import { useAppContext } from '@/context/app-context'
+import { getUserInfo } from '@/app/api/user'
 
 export type IDevelopProps = {
   params: { appId: string }
 }
 
-const Overview = ({
+const Overview = async ({
   params: { appId },
 }: IDevelopProps) => {
-  const { userProfile } = useAppContext()
+  const userProfile = await getUserInfo()
 
   return (
     <div className="h-full px-4 sm:px-16 py-6 overflow-scroll">
       <ApikeyInfoPanel />
       <TracingPanel />
       {/* takin command: 管理员可以使用api，隐藏CardView */}
-      {userProfile.role === 'admin' && (
+      {userProfile?.role === 'admin' && (
         <CardView appId={appId} />
       )}
       <CardView appId={appId} />
